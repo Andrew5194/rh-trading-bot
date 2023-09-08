7ab9dc5a2735edcf577421ded73f3b5731b14974
Andrew Yang <andrew.yang5194@gmail.com>
2023-09-08 01:17:33 -0400

    pinning dependency versions

--- rh-trading-bot.py
+++ rh-trading-bot.py
@@ -1,7 +1,4 @@
-import robin_stocks.robinhood as rh
+from pyrh import Robinhood
 
-login_response = rh.authentication.login(store_session=False)
-
-account_profile_response = rh.profiles.load_account_profile()
-print(f"Your current cash account balance is: {account_profile_response['cash_balances']['cash']}")
-
+rh = Robinhood(username="", password="")
+rh.login()
