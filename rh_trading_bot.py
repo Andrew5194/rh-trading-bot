7afacd7a3859bc82939a2a98d2c985a66298ea6e
Andrew Yang <andrew.yang5194@gmail.com>
2023-09-18 22:42:17 -0400

    adding in pytest

--- rh_trading_bot.py
+++ rh_trading_bot.py
@@ -13,15 +13,31 @@ from slack_sdk.errors import SlackApiError
 
 logging.basicConfig(level=logging.DEBUG)
 
-rh = Robinhood(username=os.getenv("RH_USERNAME"), password=os.getenv("RH_PASSWORD"))
-rh.login()
-account_profile = rh.get_account()
 
-client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
-try:
-    response = client.chat_postMessage(
-        channel=os.getenv("SLACK_USER_ID"),
-        text=f"Your portfolio cash account balance is {account_profile['portfolio_cash']}",
-    )
-except SlackApiError as e:
-    print(f"Got an error: {e.response['error']}")
+def bot_login():
+    rh = Robinhood(username=os.getenv("RH_USERNAME"), password=os.getenv("RH_PASSWORD"))
+    rh.login()
+
+    if rh.authenticated:
+        logging.info("Successfully logged in!")
+        return rh
+
+
+def bot_slack_message(rh: Robinhood):
+    if rh is not Robinhood:
+        logging.info(
+            "Missing Robinhood object. Did you make sure to log in successfully?"
+        )
+        return
+    client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
+    try:
+        response = client.chat_postMessage(
+            channel=os.getenv("SLACK_USER_ID"),
+            text=f"Your portfolio cash account balance is {rh.get_account()['portfolio_cash']}",
+        )
+    except SlackApiError as e:
+        print(f"Got an error: {e.response['error']}")
+
+
+rh = bot_login()
+bot_slack_message(rh)
